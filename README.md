8ef0b8ca6c6290be12bd8a4ed286dea5636175cc
RafaelAlexandre007 <rafael.alexandre013@gmail.com>
2022-12-29 19:33:19 -0300

    renderRoles, renderData and clearSelection

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Bootcamp-Desenvolvedor-Node
+
+- Fundamentos do Bootcamp de Desenvolvimento Front End XPE|IGTI
+  Desafio guiado: Implementar CRUD completo
--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let employees = [];
 let roles = [];
+let selectedItem;
 const listEl = document.querySelector("ul");
-const formEl = document.querySelector("form")
+const formEl = document.querySelector("form");
+const bdelete = document.getElementById("bdelete");
+const bcancel = document.getElementById("bcancel");
+const bsubmit = document.getElementById("bsubmit");
 
 async function init() {
     try {
@@ -9,7 +13,10 @@ async function init() {
             listEmployees(),
             listRoles(),
         ]);
+        renderRoles();
         rednderData();
+        clearSelection();
+        bcancel.addEventListener("click", clearSelection);
 
     } catch (error) {
         showError(error);
@@ -19,6 +26,34 @@ async function init() {
 init();
 
 //Renderizando a lista com os elementos
+function selectItem(employee, li) {
+    clearSelection();
+    selectedItem = employee;
+    li.classList.add("selected");
+
+    formEl.name.value = employee.name;
+    formEl.salary.valueAsNumber = employee.salary;
+    formEl.role_id.value = employee.role_id;
+
+    bdelete.style.display = "inline";
+    bcancel.style.display = "inline";
+
+}
+
+function clearSelection() {
+    selectedItem = undefined;
+    const li = listEl.querySelector(".selected");
+    if (li) {
+        li.classList.remove("selected");
+    }
+
+    formEl.name.value = "";
+    formEl.salary.value = "";
+    formEl.role_id.value = "";
+    bdelete.style.display = "none";
+    bcancel.style.display = "none";
+
+}
 function rednderData() {
 
     for (const employee of employees) {
@@ -31,6 +66,16 @@ function rednderData() {
         li.appendChild(divName);
         li.appendChild(divRole);
         listEl.appendChild(li);
+        li.addEventListener("click", () => selectItem(employee, li));
+    }
+}
+
+function renderRoles() {
+    for (const role of roles) {
+        const option = document.createElement("option");
+        option.textContent = role.name;
+        option.value = role.id;
+        formEl.role_id.appendChild(option);
     }
 }
 
